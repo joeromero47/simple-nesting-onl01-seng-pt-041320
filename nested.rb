543c1a1ed3fee34ed00127ce80acbd462f02f1a3
Joseph Romero <joeromero47@gmail.com>
2020-04-25 18:55:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,12 @@ def adding_matz
           :languages => ["C"]
         },
      }
+ programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "Ruby",
+    languages:
+      ["LISP", "C"]
+    }
   programmer_hash
-  programmer_hash.dig(:yukihiro_matsumoto)
 end
 
 def changing_alan

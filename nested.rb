04ac488db854ae7a84b9037f6eee223605dd04b4
Joseph Romero <joeromero47@gmail.com>
2020-04-25 18:54:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,11 +80,8 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         },
-        :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
-        }
      }
+  programmer_hash
   programmer_hash.dig(:yukihiro_matsumoto)
 end
 
